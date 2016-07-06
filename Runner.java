9b5875d4832c9923e22137a19b932a4cbd084b32
Lifyrenko Anton <alifyrenko@gmail.com>
2016-07-06 16:31:22 +0300

    update

---
+++ Runner.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * Created by ANTON on 06.07.2016.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.runGame();
+    }
+}
