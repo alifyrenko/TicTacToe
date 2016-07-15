5b23a86713dd2346a1c2727aa639f984e044987b
Andrii <tramon@users.noreply.github.com>
2016-07-15 22:29:31 +0300

    Added WeightList class
    Added WeightList class

---
+++ WeightList.java
@@ -0,0 +1,17 @@
+package test;
+
+/**
+ * Class represents a list of indexes of intField and weights for each index.
+ * @author Tramon on 08.07.2016.
+ *
+ */
+class WeightList {
+    int index;
+    int weight;
+
+
+    WeightList( int index, int weight ) {
+        this.index = index;
+        this.weight = weight;
+    }
+}
