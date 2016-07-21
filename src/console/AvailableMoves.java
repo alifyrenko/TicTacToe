fd740f3d9d9cb52727f65ae5067f3cdb1f38ead0
Lifyrenko Anton <alifyrenko@gmail.com>
2016-07-21 12:56:39 +0300

    update

--- src/console/AvailableMoves.java
+++ src/console/AvailableMoves.java
@@ -4,35 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static console.Constants.COMPUTER_SIGN_O;
+import static console.Constants.HUMAN_SIGN_X;
 
 /**
  * Created by ANTON on 21.07.2016.
  */
 public class AvailableMoves {
-     List<String> usedMoves = new ArrayList<>();
+    List<String> usedMoves = new ArrayList<>();
 
     boolean isAvailableMove(String humanMove) {
         int move;
         try {
             move = Integer.parseInt(humanMove);
-        } catch (NumberFormatException e) {
-            System.out.println("Pls, use only numbers you see on the table.");
-            return false;
-        }
+            if (GameField.squares[move].equals(COMPUTER_SIGN_O) || GameField.squares[move].equals(HUMAN_SIGN_X)) {
+                System.out.println("Field is already used by computer!");
+                return false;
 
-        if (GameField.squares[move].equals(COMPUTER_SIGN_O)){
-            System.out.println("Field is already used by computer!");
-            return false;
-        } else if (move > 8 || move < 0) {
+            } else {
+                usedMoves.add(humanMove);
+                return true;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Pls, use only numbers you see on the table.");
             return false;
-        } else if (usedMoves.contains(humanMove)) {
-            System.out.println("You've already used the move '" + humanMove + "'");
-            System.out.println("Pls, use only numbers you see on the table.");
+        } catch (NumberFormatException e){
+            System.out.println("Pls, use only numbers");
             return false;
-        } else {
-            usedMoves.add(humanMove);
-            return true;
         }
     }
 }
